602f93f3c4f88a327b35167dfc672652566e0bb7
Pariharpp <pariharprahalad26@gmail.com>
2018-12-09 02:03:07 +0530

    Updated

---
+++ src/components/Userform.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h3 class="text-info">Hello World!</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Userform"
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+import Userform from './components/Userform.vue'
+
+
+Vue.component('user-form', Userform)
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
